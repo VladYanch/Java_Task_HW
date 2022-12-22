daffdf4c2a5c6a0877fc6e6e10708cfad3c4624e
Vladyslav Yanchenko <vyanch72@gmail.com>
2022-12-22 18:33:19 +0100

    12.22 Задача на доминанту

--- JTH_1222/src/Main.java
+++ JTH_1222/src/Main.java
@@ -8,16 +8,16 @@ public class Main {
 //        Найти элемент (если он есть), который  встречается в массиве >length/2 (доминантный элемент).
 //        В противном случае вернуть -1
         int[] arr = new int[]{2, 8, 3, 3, 3, 3, 3, 3, 6, 8};
-        System.out.println(repElFinder(arr)); // вернет 3
+        System.out.println(domElFinder(arr)); // вернет 3
 
         int[] arr1 = new int[] {4, 2, 4, 1, 4, 4, 3, 5, 4, 4};
-        System.out.println(repElFinder(arr1)); // вернет 4
+        System.out.println(domElFinder(arr1)); // вернет 4
 
         int[] arr2 = new int[]{4, 3, 4, 3, 3, 4, 3, 3, 4, 4};
-        System.out.println(repElFinder(arr2)); // вернет -1
+        System.out.println(domElFinder(arr2)); // вернет -1
     }
 
-    public static int repElFinder(int[] arr) {
+    public static int domElFinder(int[] arr) {
         Map<Integer,Integer> map = new HashMap<>();
 
         for (int i = 0; i < arr.length; i++) {
